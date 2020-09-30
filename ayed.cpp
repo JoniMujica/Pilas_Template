fa3199ed0219f55693ed700ad1b0a0807c313cf0
JoniMujica <57498116+JoniMujica@users.noreply.github.com>
2020-09-30 14:08:34 -0300

    termine el ejercicio5

--- ayed.cpp
+++ ayed.cpp
@@ -1,6 +1,6 @@
 /*
-Dada una pila y un valor I, desarrollar un procedimiento que elimine los 2 primeros nodos de la pila y deje el valor I 
-como primero. (Definir parámetros y codificar).
+Dada una pila y un valor X, desarrollar un procedimiento que inserte el valor X en la última posición de la pila y la retorne. 
+(Definir parámetros y codificar).
 */
 #include <iostream>
 #include "funciones.h"
@@ -8,30 +8,47 @@ como primero. (Definir parámetros y codificar).
 using namespace std;
 
 template <typename T>
-void Eliminar(Nodo<T>*&, T);
+void ModificarVal(Nodo<T>*&, Nodo<T>*&, T);
+template <typename T>
+void Imprimir(Nodo<T>*&);
 
 int main() {
 	Nodo<int>* Pila = NULL;
+	Nodo<int>* aux = NULL;
 
 	for (int i = 0; i < 10; i++)
 	{
 		Push(Pila, i);
 	}
-	Eliminar(Pila, 150);
-	while (Pila != NULL)
-	{
-		cout << "Elementos de la pila: " << Pop(Pila) << endl;
-	}
+	int a;
+	cout << "INgrese un elemento a la pila: " << endl;
+	cin >> a;
+
+	ModificarVal(Pila, aux, a);
+	Imprimir(Pila);
 	return 0;
 }
 
-
 template <typename T>
-void Eliminar(Nodo<T>*& Pila, T x) {
-	for (int i = 0; i < 2; i++)
+void ModificarVal(Nodo<T>*& pila, Nodo<T>*& aux, T val) {
+
+	while (pila != NULL)
 	{
-		Pop(Pila);
+		T valor = Pop(pila);
+		Push(aux, valor);
 	}
-	Push(Pila, x);
-	return;
+	Push(pila, val);
+	while (aux != NULL)
+	{
+		T valor = Pop(aux);
+		Push(pila, valor);
+	}
+}
+template <typename T>
+void Imprimir(Nodo<T>*& pila) {
+	while (pila != NULL)
+	{
+		T var = Pop(pila);
+		cout << "Elementos de la pila: " << var << endl;
+	}
 }
