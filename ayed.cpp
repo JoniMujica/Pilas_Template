95c98fcd8c6db9d3a299b9519ef7e657912bc947
JoniMujica <jonimujica3@hotmail.com>
2020-10-17 21:45:24 -0300

    agrego la funcion para verificar que sean inversas

--- ayed.cpp
+++ ayed.cpp
@@ -8,12 +8,14 @@ Definir una función INVERSA que evalúe dos conjuntos de caracteres separados por
 using namespace std;
 template <typename T>
 void Ordenar(Nodo<T>*&, Nodo<T>*&);
+template <typename T>
+bool Inversa(Nodo<T>*&, Nodo<T>*&);
 
 int main() {
 	Nodo<char>* Pila1 = NULL;
 	Nodo<char>* Pila2 = NULL;
 	Ordenar(Pila1, Pila2);
-
+	Inversa(Pila1, Pila2);
 	return 0;
 }
 template <typename T>
@@ -42,4 +44,18 @@ void Ordenar(Nodo<T>*& Pila1, Nodo<T>*& Pila2) {
 			cout << "================== Agrego el valor a Pila 2 ================" << endl;
 		}
 	}
+}
+
+template <typename T>
+bool Inversa(Nodo<T>*& Pila1, Nodo<T>*& Pila2) {
+	char val;
+	Nodo<T>* aux1 = Pila1;
+	Nodo<T>* aux2 = Pila2;
+	while (Pila1 != NULL && Pila2 != NULL)
+	{
+		val = Pop(aux1); //como tienen que se inersas, a la pila1 lo saco como el ultimo que salio, y al primero
+		if (val == aux2->info)
+			return true;
+	}
+	return false;
 }
