8b41969ddb226ed892a0a9a2f486348e80eba422
JoniMujica <jonimujica3@hotmail.com>
2020-10-17 21:29:22 -0300

    agrego un ciclo de repeticion para adicion de elementos de pila2 y arreglo la funcion Push del bucle Pila1

--- ayed.cpp
+++ ayed.cpp
@@ -6,13 +6,40 @@ Definir una función INVERSA que evalúe dos conjuntos de caracteres separados por
 #include "funciones.h"
 
 using namespace std;
-
 template <typename T>
+void Ordenar(Nodo<T>*&, Nodo<T>*&);
 
 int main() {
 	Nodo<char>* Pila1 = NULL;
 	Nodo<char>* Pila2 = NULL;
-
+	Ordenar(Pila1, Pila2);
 
 	return 0;
+}
+template <typename T>
+void Ordenar(Nodo<T>*& Pila1, Nodo<T>*& Pila2) {
+	char val;
+	int contador = 0;
+	do //utilizo 'do' por que almenos una vez se tiene que introducir un valor
+	{
+		cout << "Ingrese un valor: " << endl;
+		cin >> val;
+		if (val != '.')
+		{
+			Push(Pila1, val);
+			cout << "================== Agrego el valor a Pila 1 ================" << endl;
+			contador++; //incremento la variable para utilizarlo en el for
+		}
+
+	} while (val != '.');
+	for (int i = 0; i < contador; i++)
+	{
+		cout << "Ingrese un valor: " << endl;
+		cin >> val;
+		if (val != '.')
+		{
+			Push(Pila2, val);
+			cout << "================== Agrego el valor a Pila 2 ================" << endl;
+		}
+	}
 }
