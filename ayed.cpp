dd3f001e9e539efd8f0bac707f85b12a725ab7bb
JoniMujica <jonimujica3@hotmail.com>
2020-10-17 23:10:48 -0300

    agrego una funcion para mostrar un texto segun el valor de la funcion bool,y termino el ejercicio

--- ayed.cpp
+++ ayed.cpp
@@ -2,6 +2,8 @@
 Definir una función INVERSA que evalúe dos conjuntos de caracteres separados por un punto y retorne True si los conjuntos son inversos 
 (ej: ABcDe.eDcBA) o False si no lo son. Los conjuntos deben ingresarse por teclado. (Definir parámetros y codificar).
 */
+
+/**/
 #include <iostream>
 #include "funciones.h"
 
@@ -10,12 +12,23 @@ template <typename T>
 void Ordenar(Nodo<T>*&, Nodo<T>*&);
 template <typename T>
 bool Inversa(Nodo<T>*&, Nodo<T>*&);
+template <typename T>
+void Mostrar_Pila(Nodo<T>*&);
 
 int main() {
 	Nodo<char>* Pila1 = NULL;
 	Nodo<char>* Pila2 = NULL;
 	Ordenar(Pila1, Pila2);
-	Inversa(Pila1, Pila2);
+	Mostrar_Pila(Pila1);
+	Mostrar_Pila(Pila2);
+	if (Inversa(Pila1, Pila2) == false) {
+		cout << "El valor es falso" << endl;
+	}
+	else
+	{
+		cout << "El valor es verdadero" << endl;
+	}
+
 	return 0;
 }
 template <typename T>
@@ -48,14 +61,33 @@ void Ordenar(Nodo<T>*& Pila1, Nodo<T>*& Pila2) {
 
 template <typename T>
 bool Inversa(Nodo<T>*& Pila1, Nodo<T>*& Pila2) {
-	char val;
+	char dat;
 	Nodo<T>* aux1 = Pila1;
 	Nodo<T>* aux2 = Pila2;
-	while (Pila1 != NULL && Pila2 != NULL)
+	while (aux1 != NULL && aux2 != NULL)
 	{
-		val = Pop(aux1); //como tienen que se inersas, a la pila1 lo saco como el ultimo que salio, y al primero
-		if (val == aux2->info)
+		dat = Pop(aux1);
+		if (dat == aux2->info)
 			return true;
+		aux2 = aux2->sig;
 	}
 	return false;
+}
+template <typename T>
+void Mostrar_Pila(Nodo<T>*& Pila) {
+	Nodo<T>* aux = Pila;
+
+	while (aux != NULL)
+	{
+		if (aux->sig != NULL)
+		{
+			cout << aux->info;
+		}
+		else
+		{
+			cout << aux->info << endl;
+		}
+		
+		aux = aux->sig;
+	}
 }
